f933534d9cb8cf6d1813c2a4801978e9a4b78821
Travis CI User <travis@example.org>
2019-03-17 22:55:07 +0000

    Documentation upload

---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["jenkins_api"] = [{text:"impl Fail for <a class=\"enum\" href=\"jenkins_api/client/enum.Error.html\" title=\"enum jenkins_api::client::Error\">Error</a>",synthetic:false,types:["jenkins_api::client_internals::errors::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
